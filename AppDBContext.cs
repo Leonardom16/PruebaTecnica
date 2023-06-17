4112f1cfa084ecc081148fc0b5fcc91d98529de3
Leonardom16 <71310127+Leonardom16@users.noreply.github.com>
2023-06-16 18:51:36 -0700

    Add files via upload

---
+++ AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonaBD.Models;
+
+namespace PersonaBD
+{
+    public class AppDBContext: DbContext
+    {
+       public DbSet<Persona> Personas { get; set; }
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) 
+        {
+        
+        
+        
+        }
+    }
+}
